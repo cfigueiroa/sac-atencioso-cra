16f9cc7495d91e08c20be6a8f1d03113141a2532
cfigueiroa <cfigueiroa@gmail.com>
2022-12-01 19:42:43 -0300

    Exercício Iniciado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  // Estados dos inputs controlados
+  const [nome, setNome] = React.useState("");
+  const [reclamacao, setReclamacao] = React.useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // Estados que indicam que o modal a ser exibido é o de erro ou sucesso
+  const [exibirModalSucesso, setExibirModalSucesso] = React.useState(false);
+  const [exibirModalErro, setExibirModalErro] = React.useState(false);
+
+  function enviarReclamacao() {
+    // Se a pessoa não preencheu os campos => Erro
+    if (!nome || !reclamacao) {
+      setExibirModalErro(true);
+      // Se preencheu os campos => sucesso
+    } else {
+      setExibirModalSucesso(true);
+    }
+  }
+
+  // Return do App
+  return (
+    <>
+      {/* Modal de sucesso */}
+      {exibirModalSucesso && (
+        <div className="modal">
+          <div className="content">
+            <div className="header">
+              <div className="title">Mensagem enviada com sucesso</div>{" "}
+              {/* Título */}
+              <button
+                className="close"
+                onClick={() => setExibirModalSucesso(false)}
+              >
+                ×
+              </button>
+            </div>
+
+            <div className="body">
+              {" "}
+              {/* Conteúdo do modal de sucesso */}
+              Obrigado por entrar em contato {nome}! Sua reclamação será{" "}
+              <strong>sim</strong> avaliada e considerada por nossa equipe!
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Modal de erro */}
+      {exibirModalErro && (
+        <div className="modal">
+          <div className="content">
+            <div className="header">
+              <div className="title">Erro</div> {/* Título */}
+              <button
+                className="close"
+                onClick={() => setExibirModalErro(false)}
+              >
+                ×
+              </button>
+            </div>
+
+            <div className="body">
+              {" "}
+              {/* Conteúdo do modal de sucesso */}
+              Por favor, insira os dados <strong>nome</strong> e{" "}
+              <strong>reclamação</strong> corretamente!
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Layout da tela em sim (título, inputs, botão) */}
+      <div className="formulario">
+        <h1>Envie sua reclamação</h1>
+        <input
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          placeholder="Nome"
+        />
+        <input
+          value={reclamacao}
+          onChange={(e) => setReclamacao(e.target.value)}
+          placeholder="Reclamação"
+        />
+        <button onClick={enviarReclamacao}>Enviar reclamação</button>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
